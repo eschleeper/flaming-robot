5cf20b770dc4c046b5d83a87068b97e6d4b77f09
Eric <ericschleeper@gmail.com>
2014-08-19 00:29:26 -0500

    debugging

--- config/initializers/twitter.rb
+++
@@ -1,8 +0,0 @@
-$twitter = Twitter::REST::Client.new do |config|
-    
-  config.consumer_key = Rails.configuration.consumer_key
-  config.consumer_secret = Rails.configuration.consumer_secret
-  config.access_token = Rails.configuration.access_token
-  config.access_token_secret = Rails.configuration.access_token_secret
-  
-end
