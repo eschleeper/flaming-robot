2bc833f9cd4404b02d8469e81fa3805fefb137cb
Eric <ericschleeper@gmail.com>
2014-09-22 00:22:36 -0500

    More tweaks to ui, guide list

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -4,7 +4,7 @@ class GuidesController < ApplicationController
   # GET /guides
   # GET /guides.json
   def index
-    @guides = Guide.all
+    @guides = Guide.paginate(:page => params[:page], :per_page => 30)
   end
 
   # GET /guides/1
